c49e99845b8dec0681eabcdb9c401d36ced46799
Riya Singh <singh.riyarajput2003@gmail.com>
2025-05-09 15:31:31 +0530

    changes

--- client/src/component/login.jsx
+++ client/src/component/login.jsx
@@ -56,7 +56,7 @@ const Login = () => {
   
 
   return (
-
+<>
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 p-5 rounded-lg shadow-md">
     <h2 className="mb-5 text-2xl text-gray-800">Login</h2>
     <form onSubmit={handleSubmit} className="flex flex-col w-72">
