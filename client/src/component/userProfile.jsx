c9b7c2e60e749a1aa17512e26202302ad2296186
Riya Singh <singh.riyarajput2003@gmail.com>
2025-06-12 17:00:27 +0530

    user and admin dashboard changed

--- client/src/component/userProfile.jsx
+++ client/src/component/userProfile.jsx
@@ -121,7 +121,7 @@ const UserProfile = ({ onLogout }) => {
       </nav>
 
       {/* Main Content */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="mx-auto px-4 sm:px-6 lg:px-0 py-8">
         <div className="flex flex-col md:flex-row gap-8">
           {/* Sidebar */}
           <div className="w-full md:w-1/4">
